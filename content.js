1133979edeef8fc9e6b92e1ca710bf95d733b80d
w0jno <filip.wojno03@o2.pl>
2025-07-28 14:46:25 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,35 @@
+document
+  .querySelectorAll('[data-testid="availability-rate-information"]')
+  .forEach((el) => {
+    const params = new URLSearchParams(window.location.search);
+    const numberOfGuests = params.get("group_adults");
+    const checkinDate = new Date(params.get("checkin"));
+    const checkoutDate = new Date(params.get("checkout"));
+
+    const diffMs = checkoutDate - checkinDate;
+
+    const diffDays = diffMs / (1000 * 60 * 60 * 24);
+    const totalPriceElement = el.querySelector(
+      '[data-testid="price-and-discounted-price"]'
+    );
+    if (el.querySelector(".price-per-guest")) {
+      el.querySelector(".price-per-guest").remove();
+    }
+    const newDiv = document.createElement("div");
+    newDiv.className = "price-per-guest";
+    newDiv.textContent =
+      "Price per guest: " +
+      (
+        parseFloat(totalPriceElement.textContent.replace(" ", "")) /
+        numberOfGuests
+      ).toFixed(2) +
+      " PLN " +
+      "\n Price per guest per night: " +
+      (
+        parseFloat(totalPriceElement.textContent.replace(" ", "")) /
+        numberOfGuests /
+        diffDays
+      ).toFixed(2) +
+      " PLN";
+    el.appendChild(newDiv);
+  });
